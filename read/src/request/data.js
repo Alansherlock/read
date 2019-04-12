ca7318d949503831988ae2a81237d41aed3f12a1
unknown <liu.zehao@dtyunxi.ltd>
2019-04-12 15:42:50 +0800

    修复数据库启动路径

--- read/src/request/data.js
+++ read/src/request/data.js
@@ -1,6 +1,7 @@
 const appId = 'wxdab527ed4a71eaef';
 // const baseUrl = 'http://factory-alpha.juniusoft.com/new-mf-fac';
 const baseUrl = 'https://alanlily.top';
+// const baseUrl = 'http://127.0.0.1:3333';
 
 
 module.exports = {
